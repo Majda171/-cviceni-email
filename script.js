3994c204c02dbe98e8e4f6ee8fece315b842cc31
Magdaléna Škobrtalová <mahdalova9@seznam.cz>
2024-11-04 19:33:39 +0100

    druhy ukol

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-
-const goodbye = (jmeno) => {
-    const closingParagraph = document.querySelector('.email__closing');
-    closingParagraph.textContent = `S pozdravem, ${jmeno}`;
+const jmeno = "Pavel Nový";
+const fillSubject = (subject) => {
+    const subjectElement = document.querySelector('.email__subject'); 
+    subjectElement.textContent = subject; 
 }
 
-goodbye('Pavel Ovesný');
+
+const fillBody = (body, name) => {
+    const bodyElement = document.querySelector('.email__body'); 
+    bodyElement.textContent = body; 
+    goodbye(name); 
+}
+
+
+const goodbye = (jmeno) => {
+    const closingParagraph = document.querySelector('.email__closing'); 
+    closingParagraph.textContent = `S pozdravem, ${jmeno}`; }
+
+
+fillSubject('Důležité oznámení'); 
+fillBody('Toto je tělo e-mailu, které obsahuje důležité informace.', 'Pavel Nový');
